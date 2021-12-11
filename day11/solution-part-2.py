9316b543712a32f24223e18aad728826316c61a3
advay168 <advay168@gmail.com>
2021-12-11 12:18:36 +0530

    Added day11 python solutions

---
+++ day11/solution-part-2.py
@@ -0,0 +1,46 @@
+with open("./input.txt") as file:
+    data = file.read().splitlines()
+
+
+def neighbours(width, height, i, j):
+    for y in [i - 1, i + 0, i + 1]:
+        for x in [j - 1, j + 0, j + 1]:
+            if 0 <= y < height and 0 <= x < width and (y, x) != (i, j):
+                yield (y, x)
+
+
+def step(grid, height, width):
+    flashed = [[False for _ in line] for line in grid]
+    flashes = 0
+    for i in range(height):
+        for j in range(width):
+            grid[i][j] += 1
+    changed = True
+    while changed:
+        changed = False
+        for i in range(height):
+            for j in range(width):
+                if flashed[i][j]:
+                    continue
+                if grid[i][j] <= 9:
+                    continue
+                flashed[i][j] = True
+                flashes += 1
+                changed = True
+                for i_, j_ in neighbours(width, height, i, j):
+                    grid[i_][j_] += 1
+    for i in range(height):
+        for j in range(width):
+            if flashed[i][j]:
+                grid[i][j] = 0
+    return flashes
+
+
+grid = [[int(x) for x in line] for line in data]
+height, width = len(grid), len(grid[0])
+i = 1
+while True:
+    if step(grid, height, width) == width * height:
+        print(i)
+        break
+    i += 1
