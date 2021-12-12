3fb5c17695edb294bb644be0a07714f3cc5fcf7e
advay168 <advay168@gmail.com>
2021-12-12 11:26:20 +0530

    Added day12 python solutions

---
+++ day12/solution-part-2.py
@@ -0,0 +1,33 @@
+with open("./input.txt") as file:
+    data = file.read().splitlines()
+
+from collections import defaultdict
+
+vertices = defaultdict(list)
+for line in data:
+    a, b = line.split("-")
+    if b != "start" and a != "end":
+        vertices[a].append(b)
+    if a != "start" and b != "end":
+        vertices[b].append(a)
+
+
+def walk(vertices, counts: defaultdict[str, int], vertex: str, ok: bool):
+    if vertex == "end":
+        return 1
+    if vertex.islower():
+        if counts[vertex] > 1:
+            return 0
+        if counts[vertex] == 1:
+            if not ok:
+                return 0
+            ok = False
+    counts[vertex] += 1
+    s = 0
+    for v in vertices[vertex]:
+        s += walk(vertices, counts, v, ok)
+    counts[vertex] -= 1
+    return s
+
+
+print(walk(vertices, defaultdict(lambda:0), "start", True))
