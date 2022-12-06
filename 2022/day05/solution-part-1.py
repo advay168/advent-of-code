b9e8db7dd04d40393139edad51b1a5a740c10c81
Advay <23453652+advay168@users.noreply.github.com>
2022-12-06 13:43:13 +0530

    Added day05 python solutions

---
+++ 2022/day05/solution-part-1.py
@@ -0,0 +1,30 @@
+import re
+
+with open("./input.txt") as file:
+    data = file.read()
+
+
+def parse_row(line):
+    return re.findall(r".(.). ?", line)
+
+
+def create_stacks(parsed_lines):
+    stacks = []
+    for column in zip(*parsed_lines):
+        stacks.append([x for x in reversed(column) if x != " "])
+    return stacks
+
+
+def parse_move(line):
+    match = re.match(r"move (\d+) from (\d+) to (\d+)", line)
+    return tuple(map(int, match.groups()))
+
+
+first, second = map(str.splitlines, data.split("\n\n"))
+parsed = [parse_row(line) for line in first[:-1]]
+stacks = create_stacks(parsed)
+
+for num, frm, to in map(parse_move, second):
+    for _ in range(num):
+        stacks[to - 1].append(stacks[frm - 1].pop())
+print("".join(s[-1] for s in stacks))
